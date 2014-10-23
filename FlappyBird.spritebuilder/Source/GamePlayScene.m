86a1e37c785e7f34436a306735394e4d6a8a4079
Nicholas Brooks <nicholas@Nicholass-MacBook-Pro.local>
2014-10-22 20:36:44 -0700

    Automatically Committed for MakeGamesWithUs

--- FlappyBird.spritebuilder/Source/GamePlayScene.m
+++ FlappyBird.spritebuilder/Source/GamePlayScene.m
@@ -18,32 +18,7 @@
 -(void)update:(CCTime)delta
 {
            // put update code here
-    for (CCNode *bush in _bushes) {
-        // move the bush
-        bush.position = ccp(bush.position.x -
-                            (character.physicsBody.velocity.x * delta), bush.position.y);
-        
-        // if the left corner is one complete width off the screen,
-        // move it to the right
-        if (bush.position.x <= (-1 * bush.contentSize.width)) {
-            bush.position = ccp(bush.position.x +
-                                2 * bush.contentSize.width, bush.position.y);
-        }
-    }
-    
-    // move and loop the clouds
-    for (CCNode *cloud in _clouds) {
-        // move the cloud
-        cloud.position = ccp(cloud.position.x -
-                             (character.physicsBody.velocity.x * delta), cloud.position.y);
-        
-        // if the left corner is one complete width off the screen,
-        // move it to the right
-        if (cloud.position.x <= (-1 * cloud.contentSize.width)) {
-            cloud.position = ccp(cloud.position.x +
-                                 2 * cloud.contentSize.width, cloud.position.y);
-        }
-    }    // Increment the time since the last obstacle was added
+        }    // Increment the time since the last obstacle was added
     timeSinceObstacle += delta; // delta is approximately 1/60th of a second
     
     // Check to see if two seconds have passed
